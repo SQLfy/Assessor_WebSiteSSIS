2acfbb57b2e0fb3c4aa5c83632239db54eb5b4e3
Katherine Totten <ktotten@douglas.co.us>
2024-08-11 19:51:54 -0600

    alter compatibility on dest

---
+++ Database Build Scripts/alter_compatibility.sql
@@ -0,0 +1,9 @@
+ALTER DATABASE [assessor_staging]
+SET COMPATIBILITY_LEVEL = 150;  -- 150 is for SQL Server 2019
+
+ALTER DATABASE [assessor_datastore]
+SET COMPATIBILITY_LEVEL = 150;  -- 150 is for SQL Server 2019
+
+ALTER DATABASE [assessor_app]
+SET COMPATIBILITY_LEVEL = 150;  -- 150 is for SQL Server 2019
+
