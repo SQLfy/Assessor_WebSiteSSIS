cc1b4426530a3298f9128ff607a62ed0f48375ea
KTottenDC <127148669+KTottenDC@users.noreply.github.com>
2024-09-05 10:35:22 -0600

    Update ROBO_CopyOption.ps1
    Latest use w updated comments.

--- Scripts/ROBO_CopyOption.ps1
+++ Scripts/ROBO_CopyOption.ps1
@@ -1,6 +1,5 @@
 # This script is used to move files from one directory to another using robocopy
 # The script also creates a log file to track the movement of the files
-# Be sure to run vs code as ADMIN
 # To run in Production change the $destinationDirectory to the appropriate server 
 # Original script by: Rick Greenfield
 # Modified by: Kate Totten 2024 February -- added multi-threading to improve performance
@@ -8,19 +7,20 @@
 
 # Create a log directory if it does not exist: Change DV, UA, or PR to the appropriate server
 # Typically the log directory is on the DESTINATION SERVER
-$logToServer = "\\UARW2-APP01\e$\Photos\DATA\"
-$logFileName = "2023Log_Appeals" #Make unique for each run
+$logToServer = "\\PRRW2-APP01\e$\Photos\DATA\"
+$logFileName = "2023Log_AppealsPR" #Make unique for each run
 
 $logDirectory = $logToServer + $logFileName #administrative drive share reference on DESTINATION SERVER
 New-Item -ItemType Directory -Path $logDirectory -Force | Out-Null
 
 $logFilePath = Join-Path $logDirectory "Log.txt"
 
-$sourceDirectory = "\\DVRW2-APP01\e$\Photos\DATA\2023"
+$sourceDirectory = "\\UARW2-APP01\e$\Photos\DATA\2023"
 Write-Host "Source file count: "+ ( Get-ChildItem -Recurse $sourceDirectory | Measure-Object ).Count;
 
-# Save off previous file directory before running unless you intend to overwrite.
-$destinationDirectory = "\\UARW2-APP01\e$\Photos\DATA\2023\"
+# STOP! Manually save off previous file directory before running unless you intend to overwrite.
+# Or make a temp directory and plan on swapping after the robocopy.
+$destinationDirectory = "\\PRRW2-APP01\e$\Photos\DATA\2023_ROBO_Temp\"
 # Create the destination directory if it doesn't exist
 if (-not (Test-Path $destinationDirectory)) {
     New-Item -ItemType Directory -Path $destinationDirectory | Out-Null
