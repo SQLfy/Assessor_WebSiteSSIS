f3a6e31e31ef0baf8ee509785701692e4b23872b
KTottenDC <127148669+KTottenDC@users.noreply.github.com>
2024-09-05 10:41:12 -0600

    Update alter_compatibility.sql
    Will move this to scripts folder

--- Database Build Scripts/alter_compatibility.sql
+++ Database Build Scripts/alter_compatibility.sql
@@ -7,3 +7,6 @@ SET COMPATIBILITY_LEVEL = 150;  -- 150 is for SQL Server 2019
 ALTER DATABASE [assessor_app]
 SET COMPATIBILITY_LEVEL = 150;  -- 150 is for SQL Server 2019
 
+ALTER DATABASE [assessor_extract]
+SET COMPATIBILITY_LEVEL = 150;  -- 150 is for SQL Server 2019
+
